3b1142251aaa4bd162e900045aa39496be149aa1
msuhyeon <job.maengsh@gmail.com>
2025-05-09 17:37:40 +0900

    feat: 프로젝트 셋팅 및 레이아웃 구성 (WIP)

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="p-4 h-screen">
+      <h1 className="text-2xl font-bold">
+        카페 누오보에 오신 걸 환영합니다 ☕
+      </h1>
+      <p>메뉴를 둘러보세요</p>
+    </main>
+  );
+}
