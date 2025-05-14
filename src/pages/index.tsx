90e73181f6910e2503168dff4c56d231e37320db
msuhyeon <job.maengsh@gmail.com>
2025-05-14 16:57:14 +0900

    feat: cta 섹션 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import MainBanner from "@/features/home/MainBanner";
 import MonthlyBestMenu from "@/features/home/MonthlyBestMenu";
+import CallToAction from "@/shared/components/CallToAction";
 
 export default function Home() {
   return (
     <main className="flex-grow">
       <MainBanner />
-      <MonthlyBestMenu />
+      <div className="container mx-auto px-30 sm:px-10 lg:px-50">
+        <MonthlyBestMenu />
+      </div>
+      <CallToAction />
     </main>
   );
 }
---
+++ src/shared/components/CallToAction.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+const CallToAction = () => {
+  return (
+    <section>
+      <Image
+        src="/images/cta_banner.png"
+        alt="cta 배너"
+        width="1920"
+        height="300"
+      />
+    </section>
+  );
+};
+
+export default CallToAction;
