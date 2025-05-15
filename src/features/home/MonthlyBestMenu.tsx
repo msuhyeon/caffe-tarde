485eee98ed4dd2e9a70a4db1e488cda1d49ad718
msuhyeon <job.maengsh@gmail.com>
2025-05-15 19:09:26 +0900

    feat: monthly best menu에 상품 상세페이지 링크 연결

--- src/features/home/MonthlyBestMenu.tsx
+++ src/features/home/MonthlyBestMenu.tsx
@@ -1,5 +1,5 @@
+import Link from "next/link";
 import Image from "next/image";
-import { Button } from "@/shared/components/ui/button";
 import {
   Card,
   CardContent,
@@ -16,7 +16,7 @@ type DrinkTypes = {
   desc: string;
   alt: string;
   price: number;
-  link?: string;
+  link: string;
 };
 
 const MonthlyBestMenu = () => {
@@ -36,7 +36,7 @@ const MonthlyBestMenu = () => {
       name: "카페 라떼",
       desc: "우유 위에 달콤쌉싸름한 휘핑 커피 크림을 얹은 음료",
       price: 4500,
-      link: "#",
+      link: "/drinks/9200000004544",
     },
     {
       src: "/images/best_02.png",
@@ -44,7 +44,7 @@ const MonthlyBestMenu = () => {
       name: "아인슈페너",
       desc: "진한 에스프레소 위에 부드러운 휘핑 크림을 얹은 오스트리아 전통 커피",
       price: 6000,
-      link: "#",
+      link: "/drinks/9200000004545",
     },
     {
       src: "/images/best_03.png",
@@ -52,7 +52,7 @@ const MonthlyBestMenu = () => {
       name: "달고나 커피",
       desc: "달고나 사탕 같은 달콤함을 살린 커피 크림을 차가운 우유 위에 얹은 음료",
       price: 5500,
-      link: "#",
+      link: "/drinks/9200000004546",
     },
     {
       src: "/images/best_04.png",
@@ -60,7 +60,7 @@ const MonthlyBestMenu = () => {
       name: "아이스 아메리카노",
       desc: "시원한 에스프레소를 얼음과 함께 깔끔하게 즐기는 커피",
       price: 4000,
-      link: "#",
+      link: "/drinks/9200000004547",
     },
   ];
 
@@ -86,7 +86,12 @@ const MonthlyBestMenu = () => {
             </CardHeader>
             <CardContent className="flex justify-between items-center">
               <p>{`${formatPrice(item.price)}원`}</p>
-              <Button variant="outline">자세히 보기</Button>
+              <Link
+                className="cursor-pointer py-2 px-4 rounded-sm border-1 text-sm"
+                href={item.link}
+              >
+                자세히 보기
+              </Link>
             </CardContent>
             <CardFooter />
           </Card>
