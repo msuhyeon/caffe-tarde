113778ffb4ee12991e952bcd899c8df6bfdbfe30
msuhyeon <job.maengsh@gmail.com>
2025-05-16 17:35:43 +0900

    refactor: 코드 포맷 변경

--- src/features/home/MonthlyBestMenu.tsx
+++ src/features/home/MonthlyBestMenu.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
-import Image from "next/image";
+import Link from 'next/link';
+import Image from 'next/image';
 import {
   Card,
   CardContent,
@@ -7,8 +7,8 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/shared/components/ui/card";
-import { formatPrice } from "@/lib/format";
+} from '@/shared/components/ui/card';
+import { formatPrice } from '@/lib/format';
 
 type DrinkTypes = {
   src: string;
@@ -20,47 +20,38 @@ type DrinkTypes = {
 };
 
 const MonthlyBestMenu = () => {
-  // const itemImages: CardItem[] = [
-  //   {
-  //     src: "/images/menu/strawberry-latte.png",
-  //     alt: "딸기라떼",
-  //     link: "/menu/drink/00000001",
-  //     desc: "달콤한 딸기와 우유의 조화, 시원하게 즐기세요!",
-  //   },
-  // ];
-
   const items: DrinkTypes[] = [
     {
-      src: "/images/best_01.png",
-      alt: "음료 사진",
-      name: "카페 라떼",
-      desc: "우유 위에 달콤쌉싸름한 휘핑 커피 크림을 얹은 음료",
+      src: '/images/best_01.png',
+      alt: '음료 사진',
+      name: '카페 라떼',
+      desc: '우유 위에 달콤쌉싸름한 휘핑 커피 크림을 얹은 음료',
       price: 4500,
-      link: "/drinks/9200000004544",
+      link: '/drinks/9200000004544',
     },
     {
-      src: "/images/best_02.png",
-      alt: "아인슈페너 커피",
-      name: "아인슈페너",
-      desc: "진한 에스프레소 위에 부드러운 휘핑 크림을 얹은 오스트리아 전통 커피",
+      src: '/images/best_02.png',
+      alt: '아인슈페너 커피',
+      name: '아인슈페너',
+      desc: '진한 에스프레소 위에 부드러운 휘핑 크림을 얹은 오스트리아 전통 커피',
       price: 6000,
-      link: "/drinks/9200000004545",
+      link: '/drinks/9200000004545',
     },
     {
-      src: "/images/best_03.png",
-      alt: "음료 사진",
-      name: "달고나 커피",
-      desc: "달고나 사탕 같은 달콤함을 살린 커피 크림을 차가운 우유 위에 얹은 음료",
+      src: '/images/best_03.png',
+      alt: '음료 사진',
+      name: '달고나 커피',
+      desc: '달고나 사탕 같은 달콤함을 살린 커피 크림을 차가운 우유 위에 얹은 음료',
       price: 5500,
-      link: "/drinks/9200000004546",
+      link: '/drinks/9200000004546',
     },
     {
-      src: "/images/best_04.png",
-      alt: "음료 사진",
-      name: "아이스 아메리카노",
-      desc: "시원한 에스프레소를 얼음과 함께 깔끔하게 즐기는 커피",
+      src: '/images/best_04.png',
+      alt: '음료 사진',
+      name: '아이스 아메리카노',
+      desc: '시원한 에스프레소를 얼음과 함께 깔끔하게 즐기는 커피',
       price: 4000,
-      link: "/drinks/9200000004547",
+      link: '/drinks/9200000004547',
     },
   ];
 
