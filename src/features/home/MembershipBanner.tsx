493b2b5a4c4be3b3ceca80f087a61ab43b9e66c1
msuhyeon <job.maengsh@gmail.com>
2025-05-16 18:30:11 +0900

    fix: 불필요한 코드 제거

--- src/features/home/MembershipBanner.tsx
+++ src/features/home/MembershipBanner.tsx
@@ -1,6 +1,6 @@
 const MembershipBanner = () => {
   return (
-    <section className="bg-[#f5f5f5] py-10 my-10 rounded-xl max-w-[1200px] mx-auto px-6 text-center">
+    <section className="bg-[#f5f5f5] py-10 my-10 rounded-xl  mx-auto text-center">
       <h3 className="text-2xl font-bold mb-3">TARDE 멤버십 "올라" 혜택</h3>
       <p className="text-neutral-600 mb-5">
         매일 커피 한 잔, 더 똑똑하게 즐기세요!
@@ -10,7 +10,7 @@ const MembershipBanner = () => {
         <li>포인트 적립 및 전용 이벤트</li>
         <li>🎁 생일 축하 쿠폰</li>
       </ul>
-      <button className="mt-6 px-6 py-2 border border-neutral-500 rounded hover:bg-neutral-100 transition cursor-pointer">
+      <button className="mt-6 px-6 py-2 border border-neutral-500 rounded hover:bg-[#505e54] hover:text-white transition cursor-pointer">
         멤버십 자세히 보기
       </button>
     </section>
--- src/features/home/MonthlyBestMenu.tsx
+++ src/features/home/MonthlyBestMenu.tsx
@@ -8,52 +8,12 @@ import {
   CardHeader,
   CardTitle,
 } from '@/shared/components/ui/card';
-import { formatPrice } from '@/lib/format';
-
-type DrinkTypes = {
-  src: string;
-  name: string;
-  desc: string;
-  alt: string;
-  price: number;
-  link: string;
-};
+// import { formatPrice } from '@/lib/format';
+import drinks from '@/data/drinks.json';
+import { DrinkDetailTypes } from '@/features/drinks/types';
 
 const MonthlyBestMenu = () => {
-  const items: DrinkTypes[] = [
-    {
-      src: '/images/best_01.png',
-      alt: '음료 사진',
-      name: '카페 라떼',
-      desc: '우유 위에 달콤쌉싸름한 휘핑 커피 크림을 얹은 음료',
-      price: 4500,
-      link: '/drinks/9200000004544',
-    },
-    {
-      src: '/images/best_02.png',
-      alt: '아인슈페너 커피',
-      name: '아인슈페너',
-      desc: '진한 에스프레소 위에 부드러운 휘핑 크림을 얹은 오스트리아 전통 커피',
-      price: 6000,
-      link: '/drinks/9200000004545',
-    },
-    {
-      src: '/images/best_03.png',
-      alt: '음료 사진',
-      name: '달고나 커피',
-      desc: '달고나 사탕 같은 달콤함을 살린 커피 크림을 차가운 우유 위에 얹은 음료',
-      price: 5500,
-      link: '/drinks/9200000004546',
-    },
-    {
-      src: '/images/best_04.png',
-      alt: '음료 사진',
-      name: '아이스 아메리카노',
-      desc: '시원한 에스프레소를 얼음과 함께 깔끔하게 즐기는 커피',
-      price: 4000,
-      link: '/drinks/9200000004547',
-    },
-  ];
+  const items: DrinkDetailTypes[] = drinks.slice(0, 4);
 
   return (
     <section className="py-10">
@@ -61,10 +21,10 @@ const MonthlyBestMenu = () => {
       <div className="flex justify-between pt-10 gap-8">
         {items.map((item, index) => (
           <Card className="flex-1 p-0 overflow-hidden rounded-sm" key={index}>
-            <div className="relative h-50 w-full">
+            <div className="relative h-70 w-full">
               <Image
-                src={item.src}
-                alt={item.alt}
+                src={item.imageSrc}
+                alt={item.imageAlt}
                 fill
                 className="object-cover"
               />
@@ -73,13 +33,13 @@ const MonthlyBestMenu = () => {
               <CardTitle className="text-xl font-semibold">
                 {item.name}
               </CardTitle>
-              <CardDescription className="">{item.desc}</CardDescription>
+              <CardDescription className="">{item.description}</CardDescription>
             </CardHeader>
             <CardContent className="flex justify-between items-center">
-              <p>{`${formatPrice(item.price)}원`}</p>
+              {/* <p>{`${formatPrice(item.price)}원`}</p> */}
               <Link
                 className="cursor-pointer py-2 px-4 rounded-sm border-1 text-sm"
-                href={item.link}
+                href={`/drinks/${item.id}`}
               >
                 자세히 보기
               </Link>
